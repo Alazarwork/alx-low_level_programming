869b3fdacaf06df5ad230a83057f942fe973c044
alazarwork <alazarworkneh@gmail.com>
2022-12-14 09:24:59 -0800

    med

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,23 @@
-int main(void)
+#include "main.h"
 
+
+
+/**
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+	
 {
 
-	    return (0);
+	print_alphabet();
+
+	return (0);
 
 }
