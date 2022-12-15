6672f5173f85a46c04b2959c36f9f23df48e108e
alazarwork <alazarworkneh@gmail.com>
2022-12-15 08:40:40 -0800

    med

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,8 @@
 /*
  *
  *  * File: 0-isupper.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
+ * Auth: Brennan D Baraban
+ */
 
 
 
@@ -13,29 +11,15 @@
 
 
 /**
- *
- *  * _isupper - Checks for uppercase characters.
- *
- *   * @c: The character to be checked.
- *
- *    *
- *
- *     * Return: 1 if character is uppercase, 0 otherwise.
- *
- *      */
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 
 int _isupper(int c)
-
 {
-
-		if (c >= 'A' && c <= 'Z')
-
-					return (1);
-
-
-
-			else
-
-						return (0);
-
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
