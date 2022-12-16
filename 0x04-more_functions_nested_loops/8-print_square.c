abeda2579046f8a5e4b5aa77b4de0e08f6ded4f6
alazarwork <alazarworkneh@gmail.com>
2022-12-15 20:54:51 -0800

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,16 +1,10 @@
 #include "holberton.h"
-
-
-
 /**
- *
  * print_square - a function that prints a square, followed by a new line
  * @size: size of both width and length
  * Return: a square made of '#'
  */
-
 void print_square(int size)
-
 {
 int co, ro;
 if (size <= 0)
