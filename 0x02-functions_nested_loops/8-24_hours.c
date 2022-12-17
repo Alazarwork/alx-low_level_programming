a8d3f5f8be5f92f6cd8301f772fdea25c18a11dd
alazarwork <alazarworkneh@gmail.com>
2022-12-17 07:29:27 -0800

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
-/** 
- * File: 8-24_hours.c
- * Auth: Brennan D Baraban
+/* 
+ *File: 8-24_hours.c
+ *Auth: Brennan D Baraban
  */
 
 #include "main.h"
 
 /**
- * jack bauer Prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59.
+ *jack_bauer - Prints every minute of the day of
+ *Jack Bauer, starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
