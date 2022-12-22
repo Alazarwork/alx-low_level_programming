0bb3fa9e7895bb28f96ee10a13fa869eefc02006
alazarwork <alazarworkneh@gmail.com>
2022-12-22 00:10:04 -0800

    m

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -18,7 +18,7 @@ char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 'M', 'N', 'O', 'P', 'Q', 'R',
 'S', 'T', 'U', 'V', 'W', 'X',
 'Y', 'Z', 'a', 'b', 'c', 'd',
-'e', 'f', 'g', 'h', 'i', 
+'e', 'f', 'g', 'h', 'i', 'j',
 'k', 'l', 'm', 'n', 'o', 'p',
 'q', 'r', 's', 't', 'u', 'v',
 'w', 'x', 'y', 'z'};
@@ -40,7 +40,7 @@ if (str[indx1] == alphabet[indx2])
 str[indx1] = rot13key[indx2];
 break;
 }
-}										
+}
 indx1++;
 }
 return (str);
